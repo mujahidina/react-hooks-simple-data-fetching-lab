331d847d9dff51a958701425fde257d63f507c85
mujahidina <mujahidabdi5070@gmail.com>
2024-02-03 20:23:05 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,23 @@
 // create your App component here
+import React, {useState, useEffect} from "react";
+
+
+function App() {
+    const [renderedDog, setRenderedDog] = useState("") 
+    const [isLoaded, setIsLoaded] = useState(false);
+  
+    useEffect(() => {
+      fetch("https://dog.ceo/api/breeds/image/random")
+        .then((response) => response.json())
+        .then((data) => {
+          setRenderedDog(data.message);
+          setIsLoaded(true);
+        });
+    }, []);
+  
+    if (!isLoaded) return <h3>Loading...</h3>;
+  
+    return <img alt="A Random Dog" src={renderedDog}/>
+  }
+
+  export default App;
